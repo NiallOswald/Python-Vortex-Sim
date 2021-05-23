eee1d4fa5238d6218425cb58c3d86970a8d41a3d
NiallOswald <79726292+NiallOswald@users.noreply.github.com>
2021-05-23 15:50:26 +0100

    Changes to VortexSim for uniformity
    Changes made to variable names and structure to match VortexStreet

--- vortexSim.py
+++ vortexSim.py
@@ -4,13 +4,13 @@ class VortexSim():
     # Class-wide imports
     import numpy as np
 
-    def __init__(self, vortex_points, x1, y1, x2, y2, strengths = None, damping = 0.0, *, step = 20):
+    def __init__(self, vortex_points, dimensions = ((-2, -2), (2, 2)), strengths = None, damping = 0.0, *, step = 20):
         """
         Parameters:
 
             vortex_points (list): A list of tuples of length 2 containing the coordinates of the vortices
 
-            x1, y1, x2, y2 (float): Floating point numbers representing the bounds of the plane that will be plotted
+            dimensions (tuple): A tuple of length 4 containing the coordinates of the corners of the plot over which the vectors are evaluated
 
             strengths (list): A list of floats which represent the strength of the vortex. A negative strength is equivalent to a negatively signed vortex
 
@@ -33,77 +33,18 @@ class VortexSim():
         ## Setting the damping factor of each vortex
         self.damping = damping
 
-        ## Setting plotting dimensions for the heatmap
-        self.extent = [x1, x2, y1, y2]
+        ## Setting the dimensions of the plot
+        self.x1, self.y1, self.x2, self.y2 = dimensions[0][0], dimensions[0][1], dimensions[1][0], dimensions[1][1]
 
         ## Setting the ratio of heatmap points to vectors
         self.step = step
 
-        ## Fixing points for the heatmap
-        x_values = self.np.linspace(x1, x2, 100*(x2 - x1))
-        y_values = self.np.linspace(y1, y2, 100*(y2 - y1))
-
-        self.x, self.y = self.np.meshgrid(x_values, y_values)
-
-        ## Fixing locations of each vector
-        self.xq = [i[::step] for i in self.x[::step]]
-        self.yq = [i[::step] for i in self.y[::step]]
-
-        ## Generating initial velocity data for heatmap
-        velocities = [self.update_velocities(self.x, self.y, i[1][0], i[1][1], self.strengths[i[0]]) for i in enumerate(vortex_points)]
-
-        dxdt, dydt = 0, 0
-        for i in velocities:
-            dxdt += i[0]
-            dydt += i[1]
-            
-        self.v = self.np.sqrt(dxdt**2 + dydt**2)
-
-        ## Generating initial velocity data for quiver plot
-        self.dxdtq = [i[::step] for i in dxdt[::step]]
-        self.dydtq = [i[::step] for i in dydt[::step]]
-
-    def save_sim(self, length = 100, interval = 50):
-        """
-        Saves a gif of the vortex simulation to filepath
-
-        Parameters:
-
-            length (int): The total number of frames present in the gif
-
-            interval (int): The number of milliseconds between frames in the gif
-        """
-
-        import matplotlib.pyplot as plt
-        from matplotlib.animation import FuncAnimation
-        from astropy.visualization import (MinMaxInterval, LogStretch, ImageNormalize)
-
-        # Setting up the plot
-        self.fig, self.ax = plt.subplots(1, 1)
-
-        # Normalising the heatmap colours using a log stretch
-        norm = ImageNormalize(self.v, interval=MinMaxInterval(), stretch=LogStretch())
-
-        # Plotting the initial heatmap
-        self.im = self.ax.imshow(self.v, origin='lower', norm=norm, extent=self.extent)
-
-        # Plotting the initial quiver plot
-        self.Q = self.ax.quiver(self.xq, self.yq, self.dxdtq, self.dydtq, pivot='mid')
-
-        # Animating the movement of the quiver plot
-        animator = FuncAnimation(self.fig, self.update_plots, fargs = (self.x, self.y, self.damping, self.step), 
-                                    frames = length, interval = interval, blit=False)
-        self.fig.tight_layout()
-
-        # Saving gif of animation
-        animator.save('vortexdiagram.gif', writer='imagemagick')
-
-    def update_velocities(self, x, y, x_vortex, y_vortex, strength):
+    def update_velocities(self, x, y, x0, y0, strength):
         'Finds velocities of a single vortex, when the vortex is translated away from the origin'
         
         # Translated point field
-        xt = x - x_vortex
-        yt = y - y_vortex
+        xt = x - x0
+        yt = y - y0
         
         # Generate new velocities
         dxdt = -strength*yt/(xt**2 + yt**2)
@@ -112,17 +53,17 @@ class VortexSim():
         # Return new velocities
         return (dxdt, dydt)
 
-    def move_vortex(self, vortex_points, movable, strengths, *, delta = 0.05):
+    def move_vortex(self, movable, delta):
         'Finds the velocity vector acting on a vortex, when that vortex is removed'
         
         # Creates a copy of vortex_points and then pops the movable vortex
-        vortex_fixed = vortex_points.copy()
+        vortex_fixed = self.vortex_points.copy()
         vortex_movable = vortex_fixed.pop(movable)
         
         xm, ym = vortex_movable[0], vortex_movable[1]
         
         # Removing the strength of the movable vortex, as to prevent iteration problems
-        fixed_strengths = strengths.copy()
+        fixed_strengths = self.strengths.copy()
         fixed_strengths.pop(movable)
         
         # Defining lambda expressions to calculate the derivative at xm, ym for each fixed vortex
@@ -138,14 +79,14 @@ class VortexSim():
         # Return updated vortex position
         return (xm + delta*dxdt, ym + delta*dydt)
 
-    def update_plots(self, num, x, y, damping, step):
-        'Updates the plots for each tick of time'
+    def update_plots(self, num, x, y, delta):
+        'Updates the plots for each frame'
 
         # Update strengths with damping factor
-        self.strengths = [x*(1 - damping) for x in self.strengths]
+        self.strengths = [x*(1 - self.damping) for x in self.strengths]
         
         # Update the positions of the vortices
-        updated_vortex_points = [self.move_vortex(self.vortex_points, i, self.strengths) for i in range(len(self.vortex_points))]
+        updated_vortex_points = [self.move_vortex(i, delta) for i in range(len(self.vortex_points))]
         
         self.vortex_points = updated_vortex_points.copy()
         
@@ -162,8 +103,8 @@ class VortexSim():
         v = self.np.sqrt(dxdt**2 + dydt**2)
         
         # Create a lower resolution list of velocities for use in the quiver plot
-        dxdtq = [i[::step] for i in dxdt[::step]]
-        dydtq = [i[::step] for i in dydt[::step]]
+        dxdtq = [i[::self.step] for i in dxdt[::self.step]]
+        dydtq = [i[::self.step] for i in dydt[::self.step]]
         
         # Update plot data
         self.Q.set_UVC(dxdtq, dydtq)
@@ -171,7 +112,71 @@ class VortexSim():
         
         # Return updated plots
         return [self.im, self.Q]
-    
+
+    def save_sim(self, frames = 100, interval = 50, delta = 0.05):
+        """
+        Saves a gif of the vortex simulation to filepath
+
+        Parameters:
+
+            frames (int): The total number of frames present in the gif
+
+            interval (int): The number of milliseconds between frames in the gif
+
+            delta (float): A float representing the size of steps between each frame
+        """
+
+        # Imports
+        import matplotlib.pyplot as plt
+        from matplotlib.animation import FuncAnimation
+        from astropy.visualization import (MinMaxInterval, LogStretch, ImageNormalize)
+
+        # Fixing points for the heatmap
+        x_values = self.np.linspace(self.x1, self.x2, 100*(self.x2 - self.x1))
+        y_values = self.np.linspace(self.y1, self.y2, 100*(self.y2 - self.y1))
+
+        x, y = self.np.meshgrid(x_values, y_values)
+
+        # Fixing locations of each vector
+        xq = [i[::self.step] for i in x[::self.step]]
+        yq = [i[::self.step] for i in y[::self.step]]
+
+        # Generating initial velocity data for heatmap
+        velocities = [self.update_velocities(x, y, i[1][0], i[1][1], self.strengths[i[0]]) for i in enumerate(self.vortex_points)]
+
+        dxdt, dydt = 0, 0
+        for i in velocities:
+            dxdt += i[0]
+            dydt += i[1]
+            
+        v = self.np.sqrt(dxdt**2 + dydt**2)
+
+        # Generating initial velocity data for quiver plot
+        dxdtq = [i[::self.step] for i in dxdt[::self.step]]
+        dydtq = [i[::self.step] for i in dydt[::self.step]]
+
+        # Setting up the plot
+        self.fig, self.ax = plt.subplots(1, 1)
+
+        # Setting plotting dimensions for the heatmap
+        extent = [self.x1, self.x2, self.y1, self.y2]
+
+        # Normalising the heatmap colours using a log stretch
+        norm = ImageNormalize(v, interval = MinMaxInterval(), stretch = LogStretch())
+
+        # Plotting the initial heatmap
+        self.im = self.ax.imshow(v, origin = 'lower', norm = norm, extent = extent)
+
+        # Plotting the initial quiver plot
+        self.Q = self.ax.quiver(xq, yq, dxdtq, dydtq, pivot = 'mid')
+
+        # Animating the movement of the quiver plot
+        animator = FuncAnimation(self.fig, self.update_plots, fargs = (x, y, delta), frames = frames, interval = interval, blit = False)
+        self.fig.tight_layout()
+
+        # Saving gif of animation
+        animator.save('vortexdiagram.gif', writer='imagemagick')
+
 class VortexStreet():
     'A pseudo-simulation for a vortex street'
 
